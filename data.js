fe2a37e73ef8e3d5f6874946f1dbeae385b5c467
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-08-29 14:49:40 +0200

    add products from a data file + changing prices

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = {
+  storename: "Utrecht wholesale",
+  productCount: 10,
+  products: [
+    { id: 0, name: "Potato",       category: "grocery", stock: 500, price: "3.95", pic_url: "http://pngimg.com/uploads/potato/potato_PNG7094.png", discount: "50%" },
+    { id: 1, name: "Cheese",       category: "fresh", stock: 5, price: "4.95", pic_url: "http://www.oudendijk.be/media/1186/gouda.png", discount: "50%" },
+    { id: 2, name: "Milk",         category: "fresh", stock: 10, price: "6.95", pic_url: "https://images-na.ssl-images-amazon.com/images/I/81uQm2SqxhL._SY606_.jpg", discount: "18%" },
+    { id: 3, name: "Ham",          category: "fresh", stock: 3, price: "1.95", pic_url: "https://az810478.vo.msecnd.net/media/images/default-source/Products/ham/family-ham-half.png", discount: "2%" },
+    { id: 4, name: "Nuts",         category: "grocery", stock: 185, price: "3.95", pic_url: "https://www.galipnuts.net/wp-content/uploads/2018/03/roasted.png", discount: "60%" },
+    { id: 5, name: "Bananas",      category: "fresh", stock: 63, price: "8.95", pic_url: "https://c7.uihere.com/files/394/142/426/banana-bread-banana-pudding-smoothie-fruit-banana-thumb.jpg", discount: "20%" },
+    { id: 6, name: "Rice",         category: "grocery", stock: 80, price: "3.95", pic_url: "http://www.quest2fitness.info/wp-content/uploads/2017/05/White-Rice-is-good-or-bad-for-health-300x218.png", discount: "30%" },
+    { id: 7, name: "Sugar",        category: "grocery", stock: 200, price: "9.95", pic_url: "http://www.kanjersingrootverbruik.nl/WebRoot/StoreNL2/Shops/64178920/5604/00FB/5A7B/CD35/8890/C0A8/2AB8/6AD7/vangilsekristalsuiker.jpg", discount: "47%" },
+    { id: 8, name: "Beer",         category: "alcohol", stock: 600, price: "1.95", pic_url: "https://cdn.shopify.com/s/files/1/2017/0991/products/Heineken_12_Pack_12_oz_Bottle_800x.jpeg", discount: "28%" },
+    { id: 9, name: "Toilet Paper", category: "grocery", stock: 300, price: "3.95", pic_url: "http://www.moolhuijsen.nl/resize/VNKN3032.jpg/320/320/toiletpapier-page-aloe-vera.jpg", discount: "8%" }
+  ]
+}
+
--- functions.js
+++ functions.js
@@ -8,47 +8,106 @@ console.log("hello from functions.js")
 // pricetag.style.margin = '100px'
 // product.appendChild(pricetag)
 
-setPrice(0,"10.00")
-setPrice(1,"3.00")
+//setPrice(0,"10.00")
+//setPrice(1,"3.00")
 
-
-function setPrice(id, price ) {
-    console.log("setPrice called w:", id, price)
+function setPrice(id, price) {
+    // console.log("setPrice called w:", id, price)
     const product = document.getElementById(id)
-    console.log("product", product)
+    //console.log("product", product)
     product.style.backgroundColor = 'red'
     const pricetag = document.createElement('p')
-    pricetag.innerHTML = price
+    pricetag.innerHTML = price.toString()
     pricetag.style.margin = '100px'
     product.appendChild(pricetag)
 }
 
-function addProduct (product) {
+function addProduct(product) {
     console.log("Add a product called w:", product)
-
-    //console.log("IM looking for name!!!!", product.name)
-    //console.log("IM looking for currency!!!!", product.currency)
-    //console.log("IM looking for currency!!!!", product.id)
-
 
     const div = document.createElement("div")
     const pTag = document.createElement("p")
     pTag.innerHTML = product.name
     div.appendChild(pTag)
     div.classList.add("product") // or div.class="product"; 
-    div.id = product.id  
+    div.id = product.id
 
     section = document.getElementById("products")
     const img = document.createElement("img")
-    
+
     div.appendChild(img)
-    img.src = product.url
+    img.src = product.pic_url
 
     section.appendChild(div)
-    setPrice(product.id, 6.99)
-
+    setPrice(product.id, product.price)
 }
 
-addProduct({id: 5,name: "potato", price: 3.95, currency: {usd: 3.95,
-                                                    eur: 2.95},
-                                                url: "https://www.irishtimes.com/polopoly_fs/1.3967277.1564062363!/image/image.jpg_gen/derivatives/box_620_330/image.jpg" })
+//addProduct({id: 5,name: "potato", price: 3.95, currency: {usd: 3.95,
+//                                                 eur: 2.95},
+//                                             url: "https://www.irishtimes.com/polopoly_fs/1.3967277.1564062363!/image/image.jpg_gen/derivatives/box_620_330/image.jpg" })
+
+//console.log('datafile', data.products[0])
+const listOfProducts = data.products
+
+// for(let i = 0; i <= listOfProducts.length; i++){
+//     console.log("products", listOfProducts[i])
+// }
+
+listOfProducts.forEach(function (item, index) {
+    //console.log("data",item, index)
+    // if (item.name != 'Beer') {
+    //     addProduct(item)
+    // }
+    // if (item.id < 2) {
+    //     discounted_item = item
+    //     discounted_item.price = parseInt(item.price) * 0.95
+    //     console.log("price of item", discounted_item)
+    //     addProduct(discounted_item)
+    // } else if (item.id > 5) {
+    //     discounted_item = item
+    //     discounted_item.price = parseInt(item.price) * 1.5
+    //     console.log("price of item", discounted_item)
+    //     addProduct(discounted_item)
+    // }
+    const scopingTest = "test"
+    switch (item.category) {
+        case "alcohol":
+            discounted_item = item
+            discounted_item.price = parseInt(item.price) * 1.75
+            console.log("price of item", discounted_item)
+            addProduct(discounted_item)
+            break;
+        case "fresh":
+            discounted_item = item
+            discounted_item.price = parseInt(item.price) * 1.3
+            console.log("price of item", discounted_item)
+            addProduct(discounted_item)
+
+            break;
+        case 'grocery':
+            const scopingTest = "something else"
+            console.log("scopingtest ", scopingTest)
+
+            discounted_item = item
+            discounted_item.price = parseInt(item.price) * 0.95
+            console.log("price of item", discounted_item)
+            addProduct(discounted_item)
+            break;
+        default:
+            console.log('this product is of the default category ')
+
+    }
+    console.log("scopingtest ", scopingTest)
+
+    while(  listOfProducts.length >= 1  ){
+        
+         let product = listOfProducts.pop()
+         console.log("product", product.name)
+         console.log("length of my array",listOfProducts.length)
+
+     }
+
+
+})
+
+
